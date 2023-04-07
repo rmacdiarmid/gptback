b53c74ed5f2ae5c349cc98fa9cbbdf36ffd76233
Ryan MacDiarmid <ryanmacdiarmid@gmail.com>
2023-04-07 13:06:29 -0700

    Index renders now

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,20 +2,54 @@ package handlers
 
 import (
 	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 )
 
-var templates = template.Must(template.ParseGlob("templates/*"))
+var templates *template.Template
 
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	t := templates.Lookup(tmpl)
+func init() {
+	var err error
+	templates, err = template.ParseGlob(filepath.Join("templates", "*"))
+	if err != nil {
+		log.Fatalf("Error parsing templates: %s", err)
+	}
+
+	log.Println("Loaded templates:")
+	for _, t := range templates.Templates() {
+		log.Printf("- %s", t.Name())
+	}
+}
+
+func renderTemplate(w http.ResponseWriter, templateName string) {
+	t := templates.Lookup(templateName)
 	if t == nil {
+		log.Printf("Template not found: %s", templateName)
 		http.Error(w, "Template not found.", http.StatusInternalServerError)
 		return
 	}
 
+	log.Printf("Rendering template: %s", templateName)
 	err := t.Execute(w, nil)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Printf("Error executing template: %v", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+	}
+}
+
+func RenderTemplateWithData(w http.ResponseWriter, templateName string, data interface{}) {
+	t := templates.Lookup(templateName)
+	if t == nil {
+		log.Printf("Template not found: %s", templateName)
+		http.Error(w, "Template not found.", http.StatusInternalServerError)
+		return
+	}
+
+	log.Printf("Rendering template: %s", templateName)
+	err := t.Execute(w, data)
+	if err != nil {
+		log.Printf("Error executing template: %v", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
 	}
 }
--- handlers/index.go
+++ handlers/index.go
@@ -1,9 +1,28 @@
 package handlers
 
 import (
+	"log"
 	"net/http"
 )
 
+type Article struct {
+	Title   string
+	Image   string
+	Preview string
+}
+
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "index.html")
+	log.Println("IndexHandler called")
+
+	// Retrieve data from the database
+	articles := []Article{
+		{Title: "The Whimsical Dragon's Guide to the Meaning of Life", Image: "/static/images/dragon1.jpg", Preview: "Once upon a time, in a land far, far away..."},
+		{Title: "Dragon Tales and the Meaning of Life", Image: "/static/images/dragon2.jpg", Preview: "Dragons are known for their powerful and often misunderstood nature..."},
+		{Title: "The Treasure of Dragon Island and the Meaning of Life", Image: "/static/images/dragon3.jpg", Preview: "Deep in the heart of the ocean lay a mysterious island known only to the most daring of dragons..."},
+	}
+
+	log.Printf("Rendering index with data: %#v", articles)
+
+	// Render the HTML template with the data
+	renderTemplateWithData(w, "index.html", articles)
 }
